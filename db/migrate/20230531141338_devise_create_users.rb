c0130d79bcbe90c222be92f6991c0330d58a968c
Muhammad Qasim <qasim-gondal>
2023-05-31 19:47:46 +0500

    auth wip

--- db/migrate/20230531141338_devise_create_users.rb
+++ db/migrate/20230531141338_devise_create_users.rb
@@ -4,7 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+       t.string :name
+      t.string :contact
+      t.string :adress
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
