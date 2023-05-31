96f7e6819ab7451e843b605ffcf6dbb64edcc6dd
Muhammad Qasim <qasim-gondal>
2023-05-31 20:15:08 +0500

    auth-devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_31_141338) do
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "price"
+    t.string "description"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -33,16 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_141338) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_134627) do
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.string "price"
-    t.string "description"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   end
 
 end
